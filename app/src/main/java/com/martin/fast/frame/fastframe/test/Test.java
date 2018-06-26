557380fc5a6b84bcf106b7aa8edc8dd051e6b2d0
martin <martin0207mfh@163.com>
2018-06-26 10:29:20 +0800

    网络请求优化

---
+++ app/src/main/java/com/martin/fast/frame/fastframe/test/Test.java
@@ -0,0 +1,20 @@
+package com.martin.fast.frame.fastframe.test;
+
+import android.os.Bundle;
+
+import com.martin.fast.frame.fastlib.base.BaseActivity;
+import com.martin.fast.frame.fastlib.base.BaseResponse;
+import com.martin.fast.frame.fastlib.retrofit.DefaultObserver;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author ：Martin
+ * @date : 2018/6/22 14:13
+ */
+public interface Test {
+
+    String Data = "data";
+
+}
